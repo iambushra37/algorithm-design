8b0162806ac6919f5ea72c2cb27c7f5d16699a3b
iambushra37 <68284904+iambushra37@users.noreply.github.com>
2020-07-26 23:27:07 +0530

    Update MyOwnLibrary.js
    added istouching

--- MyOwnLibrary.js
+++ MyOwnLibrary.js
@@ -10,3 +10,13 @@ function bounceOff(object1,object2){
     object2.velocityY = object2.velocityY * (-1);
   }
   }
+function isTouching(thing1, thing2) {
+  if (thing1.x - thing2.x < thing1.width / 2 + thing2.width / 2
+    && thing2.x - thing1.x < thing1.width / 2 + thing2.width / 2
+    && thing1.y - thing2.y < thing2.height / 2 + thing1.height / 2
+    && thing2.y - thing1.y < thing2.height / 2 + thing1.height / 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
